2807ba42c2365afe51c8441c5d93dfc3359604db
Faiyaz uddin <faiyaz.uddin2021@iem.edu.in>
2026-09-22 17:22:09 +0530

    pushing the app

---
+++ cal-app/test_calculator.py
@@ -0,0 +1,23 @@
+from calculator import add, subtract, multiply, divide
+import pytest
+
+
+def test_add():
+    assert add(10, 5) == 15
+
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+
+
+def test_multiply():
+    assert multiply(10, 5) == 50
+
+
+def test_divide():
+    assert divide(10, 5) == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
